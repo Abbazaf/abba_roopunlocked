6d47a8f0e2d34ac1009f868257e44b456f921e2c
Abbazaf <165559713+Abbazaf@users.noreply.github.com>
2024-04-12 22:01:26 +0500

    Update swapper.py

--- roop/swapper.py
+++ roop/swapper.py
@@ -1,4 +1,3 @@
-
 import os
 from tqdm import tqdm
 import cv2
@@ -7,9 +6,9 @@ import threading
 import roop.globals
 from roop.analyser import get_face_single, get_face_many
 
+# Preload face swapper model
 FACE_SWAPPER = None
 THREAD_LOCK = threading.Lock()
-
 
 def get_face_swapper():
     global FACE_SWAPPER
@@ -19,12 +18,50 @@ def get_face_swapper():
             FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=roop.globals.providers)
     return FACE_SWAPPER
 
+# Batch processing of frames
+def multi_process_frame(source_img, frame_paths, progress):
+    threads = []
+    num_threads = roop.globals.gpu_threads
+    num_frames_per_thread = len(frame_paths) // num_threads
+    remaining_frames = len(frame_paths) % num_threads
+
+    # Preload source face
+    source_face = get_face_single(cv2.imread(source_img))
+
+    # create thread and launch
+    start_index = 0
+    for _ in range(num_threads):
+        end_index = start_index + num_frames_per_thread
+        if remaining_frames > 0:
+            end_index += 1
+            remaining_frames -= 1
+        thread_frame_paths = frame_paths[start_index:end_index]
+        thread = threading.Thread(target=process_frames_batch, args=(source_face, thread_frame_paths, progress))
+        threads.append(thread)
+        thread.start()
+        start_index = end_index
+
+    # threading
+    for thread in threads:
+        thread.join()
+
+# Process frames in a batch
+def process_frames_batch(source_face, frame_paths, progress):
+    for frame_path in frame_paths:
+        frame = cv2.imread(frame_path)
+        try:
+            result = process_faces(source_face, frame)
+            cv2.imwrite(frame_path, result)
+        except Exception as exception:
+            print(exception)
+            pass
+        if progress:
+            progress.update(1)
 
 def swap_face_in_frame(source_face, target_face, frame):
     if target_face:
         return get_face_swapper().get(frame, target_face, source_face, paste_back=True)
     return frame
-
 
 def process_faces(source_face, target_frame):
     if roop.globals.all_faces:
@@ -37,7 +74,6 @@ def process_faces(source_face, target_frame):
         if face:
             target_frame = swap_face_in_frame(source_face, face, target_frame)
     return target_frame
-
 
 def process_frames(source_img, frame_paths, progress=None):
     source_face = get_face_single(cv2.imread(source_img))
@@ -52,31 +88,6 @@ def process_frames(source_img, frame_paths, progress=None):
         if progress:
             progress.update(1)
 
-
-def multi_process_frame(source_img, frame_paths, progress):
-    threads = []
-    num_threads = roop.globals.gpu_threads
-    num_frames_per_thread = len(frame_paths) // num_threads
-    remaining_frames = len(frame_paths) % num_threads
-
-    # create thread and launch
-    start_index = 0
-    for _ in range(num_threads):
-        end_index = start_index + num_frames_per_thread
-        if remaining_frames > 0:
-            end_index += 1
-            remaining_frames -= 1
-        thread_frame_paths = frame_paths[start_index:end_index]
-        thread = threading.Thread(target=process_frames, args=(source_img, thread_frame_paths, progress))
-        threads.append(thread)
-        thread.start()
-        start_index = end_index
-
-    # threading
-    for thread in threads:
-        thread.join()
-
-
 def process_img(source_img, target_path, output_file):
     frame = cv2.imread(target_path)
     face = get_face_single(frame)
@@ -84,7 +95,6 @@ def process_img(source_img, target_path, output_file):
     result = get_face_swapper().get(frame, face, source_face, paste_back=True)
     cv2.imwrite(output_file, result)
     print("\n\nImage saved as:", output_file, "\n\n")
-
 
 def process_video(source_img, frame_paths):
     do_multi = roop.globals.gpu_vendor is not None and roop.globals.gpu_threads > 1
